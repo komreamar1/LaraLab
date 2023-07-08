ddee6c4765f8b5108621660b8201cc740b96faf3
komreamar1 <komreamar1@gmail.com>
2023-07-08 21:18:31 +0530

    info

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>documentation</title>
+    {{-- ... --}}
+ 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-	<h1>documentation</h1>
-</body>
-</html>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+const host = 'my-app.test'; 
 
 export default defineConfig({
+    // server: { 
+    //     host, 
+    //     hmr: { host:'localhost' }, 
+    //     https: { 
+    //         key: fs.readFileSync(`/path/to/${host}.key`), 
+    //         cert: fs.readFileSync(`/path/to/${host}.crt`), 
+    //     }, 
+    // }, 
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/ts/app.tsx'],
+           // valetTls: 'my-app.test', 
             refresh: true,
-        }),
+                }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+            }),
+        react(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/ts',
+        },
+    },
 });

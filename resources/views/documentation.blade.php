cd40810d2b156b84b387330acf5803bad39f0f2e
komreamar1 <komreamar1@gmail.com>
2023-07-10 18:03:29 +0530

    live document class

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -1,9 +1,26 @@
-<!doctype html>
-<head>
-    {{-- ... --}}
- 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body>
-</body>
+        <title id="wecomeTittle"> </title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script type="text/javascript">
+                let wecomeTittle = document.getElementById('wecomeTittle');
+                let pageInformation = ["🧑🏼‍🔬 Scientific Universe 🍷","{{$meta}}"];
+                function taggleTittleInnerHtml (argument=pageInformation) {
+                    wecomeTittle.innerHTML = pageInformation[Math.floor(Math.random() * pageInformation.length)]  
+                }
+                setInterval(taggleTittleInnerHtml, 1000);
+        </script>
+        <!-- Styles -->
+        @viteReactRefresh
+        @vite('resources/act/app.tsx')
+           <body>
+               
+           </body>
+</html>

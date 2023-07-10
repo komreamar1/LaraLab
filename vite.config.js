21dd23824458d1111ad719bff32927b9debbe63c
komreamar1 <komreamar1@gmail.com>
2023-07-10 12:57:34 +0530

    react to vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.ts'
+                'resources/js/app.ts',
+                'resources/act/app.jsx'
             ],
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
+        react(),
         vue({
             template: {
                 transformAssetUrls: {
